82b810d89e5c0c839ba02deea1ae191c5c03d0a4
SaadatAzimova <saadatazimova5@gmail.com>
2024-09-27 22:34:51 +0600

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        PersonalAccount account1 = new PersonalAccount(230107002, "saadat");
+        account1.deposit(300);
+        account1.withdraw(150);
+        System.out.println(account1.getBalance());
+        account1.printTransactionHistory();
+        System.out.println(account1.getAccountHolder());
+        System.out.println(account1.getAccountNumber());
+
+
+
+        PersonalAccount account2 = new PersonalAccount(228715002, "aybike");
+        account2.deposit(200);
+        account2.withdraw(500);
+        System.out.println(account2.getBalance());
+        account2.printTransactionHistory();
+        System.out.println(account2.getAccountHolder());
+        System.out.println(account2.getAccountNumber());
+
+
 
     }
 }
--- src/PersonalAccount.java
+++ src/PersonalAccount.java
@@ -1,32 +1,37 @@
 import java.util.ArrayList;
 
-public class PesonalAccount {
+public class PersonalAccount {
     public int accountNumber;
     public String accountHolder;
     public double balance;
     private ArrayList<Amount> transactions;
 
     //constructor
-    public PesonalAccount(int accountNumber, String accountHolder) {
+    public PersonalAccount(int accountNumber, String accountHolder) {
         this.accountNumber = accountNumber;
         this.accountHolder = accountHolder;
         this.balance = 0.0;
         this.transactions = new ArrayList<Amount>();
     }
 
-    void deposit(double amount) {
+    public void deposit(double amount) {
         Amount myAmount1 = new Amount(amount, TransactionType.DEPOSIT);
         transactions.add(myAmount1);
         this.balance += amount;
     }
 
-    void withdraw(double amount) {
+    public void withdraw(double amount) {
         Amount myAmount2 = new Amount(amount, TransactionType.WITHDRAWAL);
-        transactions.add(myAmount2);
-        this.balance -= amount;
+        if(amount > balance) {
+            System.out.println("Insufficient Balance");
+        }
+        else {
+            transactions.add(myAmount2);
+            this.balance -= amount;
+        }
     }
 
-    void printTransactionHistory() {
+    public void printTransactionHistory() {
         System.out.println("Transaction History:");
         for (Amount transaction : transactions) {
             System.out.println(transaction);
